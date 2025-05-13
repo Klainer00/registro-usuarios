a4987f12039b47b732f3c547cf607cd3e14f7953
bryan <br.jaraz@duocuc.cl>
2025-05-13 11:25:14 -0400

    modelo registro usuario

---
+++ src/main/java/com/perfumeria/registro_usuario/model/UsuarioModel.java
@@ -0,0 +1,43 @@
+package com.perfumeria.registro_usuario.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name =  "usuario")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // para marcar que la id sea auto increment
+    @Column(unique = true)//indica el atributo de abajo
+    private int id;
+    @Column (length = 30, nullable = false)
+    private String nombre;
+    @Column(length = 20, nullable = false)
+    private String usuario;
+    @Column(length = 40, nullable = false)
+    private String contrasenna;
+    @Column(length = 13 , nullable = false)
+    private String rut;
+    @Column(length = 50, nullable =false)
+    private String correo;
+    @Column(length = 50, nullable =false)
+    private String dereccion;    
+    @Column(length = 50, nullable =false)
+    private String apellido;
+    @Column(nullable = false)
+    private int perfil;
+
+}
